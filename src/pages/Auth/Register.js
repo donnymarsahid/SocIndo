e3616ead63239865d9cb768efcd8c7d5e5eac324
donymgindotech <dony.mgindotech@gmail.com>
2023-04-01 17:13:15 +0700

    Progress Sttic Apps 100%

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -1,11 +1,30 @@
 import { useNavigation } from '@react-navigation/native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Alert, Image, Pressable, StyleSheet, Text, TextInput, View} from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 import logoVertical from '../../assets/img/logo_vertical.png'
+import view from '../../assets/img/view.png'
+import hide from '../../assets/img/hide.png'
 import ButtonStyle1 from '../../components/buttons/ButtonStyle1'
 
-const Register = () => {
-    const navigation = useNavigation();
+const Register = () => { 
+        const navigation = useNavigation();
+        const [formData, setFormData] = useState({
+            fullname: '',
+            username: '',
+            password: '',
+          });
+        
+        const [hidePassword, setHidePassword] = useState(true);
+        const _inputTextHandler = (key, val) => {
+            setFormData({
+              ...formData,
+              [key]: val,
+            });
+          };
+
+          const _btnToggleHandler = () => setHidePassword(!hidePassword);
+        
 
         const handlePressLogin = async () => {
           try{
@@ -15,23 +34,21 @@ const Register = () => {
            console.log(err)
           }
         }
-        const showAlert = () => {
+
+        const _btnSubmitHandler = async () => {
             Alert.alert(
-              '500',
-              'Internal Server Error',
-              [
-                {
-                  text: 'Tutup',
-                  onPress: () => console.log('Tombol Tutup Ditekan')
-                },
-                {
-                  text: 'Batal',
-                  onPress: () => console.log('Tombol Batal Ditekan'),
-                  style: 'cancel'
-                }
-              ]
-            );
-          }
+                '500',
+                'Internal server error!',
+                [
+                  {
+                    text: 'Tutup',
+                    onPress: () => console.log('Tombol Tutup Ditekan')
+                  } 
+                ]
+              );
+          return;
+          };
+        
     return (
         <View style={{backgroundColor: '#fff'}}>
         <View style={Style.container}>
@@ -39,14 +56,56 @@ const Register = () => {
                 <Image source={logoVertical} style={Style.logoImg} />
             </View>
             <View style={Style.formInput}>
-                <TextInput placeholder="Full Name" style={Style.inputText} />
-                <TextInput placeholder="Username" style={[Style.inputText, {marginTop: 15}]} />
-                <TextInput placeholder="Password" style={[Style.inputText, {marginTop: 15}]} />
             </View>
+                {/* Username Start */}
+                <View style={Style.formGroup}>
+              <View style={Style.formInputCst}>
+                <TextInput
+                  style={Style.formInputBox}
+                  placeholder="fullname"
+                  autoCapitalize={'none'}
+                  onChangeText={text => _inputTextHandler('fullname', text)}
+                  value={formData.fullname}
+                /> 
+              </View>
+            </View>
+                {/* Username End */}
+                {/* Username Start */}
+                <View style={Style.formGroup}>
+              <View style={Style.formInputCst}>
+                <TextInput
+                  style={Style.formInputBox}
+                  placeholder="username"
+                  autoCapitalize={'none'}
+                  onChangeText={text => _inputTextHandler('username', text)}
+                  value={formData.username}
+                /> 
+              </View>
+            </View>
+                {/* Username End */}
+                {/* Password Start */}
+                <View style={Style.formGroup}>
+              <View style={Style.formInputCst}>
+                <TextInput
+                  style={Style.formInputBox}
+                  placeholder="password"
+                  autoCapitalize={'none'}
+                  secureTextEntry={hidePassword}
+                  onChangeText={text => _inputTextHandler('password', text)}
+                  value={formData.password}
+                />
+                <TouchableOpacity onPress={_btnToggleHandler}>
+                  {
+                    hidePassword ? <Image source={view} style={Style.img_icon} /> : <Image source={hide} style={Style.img_icon} />
+                  }
+                </TouchableOpacity>
+              </View>
+            </View>
+                {/* Password End */}
             <View style={{marginTop: 120}}></View>
-            <ButtonStyle1 text="DAFTAR" onPress={showAlert}  />
+            <ButtonStyle1 text="DAFTAR" onPress={_btnSubmitHandler} />
             <View style={Style.desc}>
-                <Text style={Style.textDefault}>Sudah memiliki Akun, </Text>
+                <Text style={Style.textDefault}>Sudah memiliki Akun,  </Text>
                 <Pressable onPress={handlePressLogin}>
                     <Text style={Style.textButton}>
                     Masuk?
@@ -102,7 +161,38 @@ const Style = StyleSheet.create({
     textButton: {
         color: 'red',
         fontFamily: 'Poppins-Medium'
-     }
+     },
+     formGroup: {
+        marginVertical: 5,
+      },
+    formInputCst: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: '#DFDFDF',
+
+      },
+     formInputBox: {
+        flex: 1,
+        fontFamily: "Poppins-Regular",
+        fontSize: 16,
+      },
+     formInputIcon: {
+        marginLeft: 5,
+        fontSize: 50,
+        color: "red",
+      },
+      img_icon: {
+        width: 30,
+        resizeMode: "contain"
+      }
+    
+    
 })
 
 export default Register
